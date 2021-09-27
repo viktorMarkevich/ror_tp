38d4880573f0b9d3b38bbd48b1ff25b203300ff9
Victor <viktor.markevich@faceit.com.ua>
2021-09-27 10:09:44 +0300

    Add model specs; generate tickets

---
+++ spec/factories/tickets.rb
@@ -0,0 +1,26 @@
+FactoryBot.define do
+  factory :ticket do
+    request_number { '09252012-00001' }
+    sequence_number { '2421' }
+    request_type { 'Normal' }
+    reqponse_due_date { '2011/07/13 23:59:59' }
+    primay_sacode { 'ZZGL103' }
+    additional_sacode { %w[ZZL01 ZZL02 ZZL03] }
+    digsite_info_wkt { 'POLYGON((-81.13390268058475 32.07206917625161,-81.14660562247929 32.04064386441295,
+                                 -81.08858407706913 32.02259853170128,-81.05322183341679 32.02434500961698,
+                                 -81.05047525138554 32.042681017283066,-81.0319358226746 32.06537765335268,
+                                 -81.01202310294804 32.078469305179404,-81.02850259513554 32.07963291684719,
+                                 -81.07759774894413 32.07090546831167,-81.12154306144413 32.08806865844325,
+                                 -81.13390268058475 32.07206917625161))' }
+
+    trait :invalid_data do
+      request_number { nil }
+      sequence_number { nil }
+      request_type { nil }
+      reqponse_due_date { nil }
+      primay_sacode { nil }
+      additional_sacode { nil }
+      digsite_info_wkt { nil }
+    end
+  end
+end
---
+++ spec/models/ticket_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Ticket, type: :model do
+  it 'has a valid factory' do
+    expect(build(:ticket)).to be_valid
+  end
+
+  context 'when test validation' do
+    let(:ticket) { build :ticket, :invalid_data }
+
+    context 'when test basic validations' do
+      it { expect(ticket).to validate_presence_of(:request_number) }
+      it { expect(ticket).to validate_presence_of(:sequence_number) }
+      it { expect(ticket).to validate_presence_of(:request_type) }
+      it { expect(ticket).to validate_presence_of(:reqponse_due_date) }
+      it { expect(ticket).to validate_presence_of(:primay_sacode) }
+      it { expect(ticket).to validate_presence_of(:additional_sacode) }
+      it { expect(ticket).to validate_presence_of(:digsite_info_wkt) }
+    end
+
+    context 'when test format validations' do
+      it { expect(ticket).not_to allow_value('09jdfk252012-00001').for(:request_number) }
+    end
+  end
+end
