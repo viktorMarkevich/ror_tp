ce91616e60f285ac8a00aa83824864faa75ad33e
Victor <viktor.markevich@faceit.com.ua>
2021-09-27 11:16:19 +0300

    Add request_number validation of format

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,4 +2,6 @@ class Ticket < ApplicationRecord
 
   validates :request_number, :sequence_number, :request_type, :reqponse_due_date, :primay_sacode, :additional_sacode,
             :digsite_info_wkt, presence: true
+  validates :request_number, format: { with: /\A\d+.-\d+\z/,
+                                       message: 'should contain only numbers and `-` symbol.' }
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -6,20 +6,36 @@ RSpec.describe Ticket, type: :model do
   end
 
   context 'when test validation' do
-    let(:ticket) { build :ticket, :invalid_data }
+    let(:invalid_ticket) { build :ticket, :invalid_data }
 
     context 'when test basic validations' do
-      it { expect(ticket).to validate_presence_of(:request_number) }
-      it { expect(ticket).to validate_presence_of(:sequence_number) }
-      it { expect(ticket).to validate_presence_of(:request_type) }
-      it { expect(ticket).to validate_presence_of(:reqponse_due_date) }
-      it { expect(ticket).to validate_presence_of(:primay_sacode) }
-      it { expect(ticket).to validate_presence_of(:additional_sacode) }
-      it { expect(ticket).to validate_presence_of(:digsite_info_wkt) }
+      it { expect(invalid_ticket).to validate_presence_of(:request_number) }
+      it { expect(invalid_ticket).to validate_presence_of(:sequence_number) }
+      it { expect(invalid_ticket).to validate_presence_of(:request_type) }
+      it { expect(invalid_ticket).to validate_presence_of(:reqponse_due_date) }
+      it { expect(invalid_ticket).to validate_presence_of(:primay_sacode) }
+      it { expect(invalid_ticket).to validate_presence_of(:additional_sacode) }
+      it { expect(invalid_ticket).to validate_presence_of(:digsite_info_wkt) }
     end
 
     context 'when test format validations' do
-      xit { expect(ticket).not_to allow_value('09jdfk252012-00001').for(:request_number) }
+      let(:valid_ticket) { build :ticket }
+
+      context 'when test request_number' do
+        it 'returns invalid for alphabetic symbols' do
+          expect(valid_ticket).not_to allow_value('09jdfk252012-00001').for(:request_number)
+                                .with_message('should contain only numbers and `-` symbol.')
+        end
+
+        it 'returns invalid for whitespace symbols' do
+          expect(valid_ticket).not_to allow_value('09 252012-00001').for(:request_number)
+                                .with_message('should contain only numbers and `-` symbol.')
+        end
+
+        it 'receives the valid value' do
+          expect(valid_ticket).to allow_value('09252012-00001').for(:request_number)
+        end
+      end
     end
   end
 end
